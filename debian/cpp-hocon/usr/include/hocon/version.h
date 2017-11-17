8492e15527f4716d565402abe2a28158238b7c9d
Isabel Amaro <isabel.bicalho@yahoo.com.br>
2017-11-17 17:03:58 -0200

    Add cpp-hocon packaging

---
+++ debian/cpp-hocon/usr/include/hocon/version.h
@@ -0,0 +1,28 @@
+///
+/// @file
+/// Declares macros for the cpp-hocon library version.
+///
+#pragma once
+
+///
+/// The cpp-hocon library major version.
+///
+#define CPP_HOCON_VERSION_MAJOR 0
+///
+/// The cpp-hocon library minor version.
+///
+#define CPP_HOCON_VERSION_MINOR 1
+///
+/// The cpp-hocon library patch version.
+///
+#define CPP_HOCON_VERSION_PATCH 6
+
+///
+/// The cpp-hocon library version as a string (without commit SHA1).
+///
+#define CPP_HOCON_VERSION "0.1.6"
+
+///
+/// The cpp-hocon library version as a string (with commit SHA1).
+///
+#define CPP_HOCON_VERSION_WITH_COMMIT "0.1.6 (commit 6962e7d185b751ddc17fd4f6f6483b2d9f9bbff1)"
